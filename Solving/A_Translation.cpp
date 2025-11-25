bb37938e0faac81c8a65ec31cf8d48f3243a13e1
Md Eyamin rahman Pranto <erpranto55@gmail.com>
2025-11-25 23:45:34 +0600

    A_Translation

---
+++ Solving/A_Translation.cpp
@@ -0,0 +1,29 @@
+//https://codeforces.com/problemset/problem/41/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define HA cout << "YES" << '\n';
+#define NA cout << "NO" << '\n';
+#define nl '\n'
+#define mod 1000000007
+#define N 500005
+ll fx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+ll fy[] = {1, -1, 0, 0, -1, 1, -1, 1};
+
+int main()
+{
+    FAST;
+    string s1, s2;
+    cin >> s1 >> s2;
+    reverse(all(s2));
+
+    if (s1 == s2)
+        HA 
+    else 
+        NA 
+    return 0;
+}
