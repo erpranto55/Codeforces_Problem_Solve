e86c6c2b8d00edfb135fc5b09609290448922fe7
Md Eyamin rahman Pranto <erpranto55@gmail.com>
2025-11-29 23:45:43 +0600

    A_Beautiful_Year

---
+++ Solving/A_Beautiful_Year.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define HA cout << "YES" << '\n';
+#define NA cout << "NO" << '\n';
+#define nl '\n'
+#define mod 1000000007
+#define N 500005
+ll fx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+ll fy[] = {1, -1, 0, 0, -1, 1, -1, 1};
+
+int main()
+{
+    FAST;
+    int x;
+    cin >> x;
+    while (true)
+    {
+        x++;
+        int a = x / 1000;
+        int b = (x / 100) % 10; 
+        int c = (x / 10) % 10;
+        int d = x % 10;
+
+        if (a != b && a != c && a != d &&
+            b != c && b != d &&
+            c != d)
+        {
+            cout << x << endl;
+            break;
+        }
+    }
+    return 0;
+}
