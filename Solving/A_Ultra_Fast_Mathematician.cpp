b291d0a6f9b39a5ea1d65ab3566e4d8ace1922fc
ER Pranto <erpranto55@gmail.com>
2025-12-18 23:25:08 +0600

    A_Ultra_Fast_Mathematician

---
+++ Solving/A_Ultra_Fast_Mathematician.cpp
@@ -0,0 +1,34 @@
+//https://codeforces.com/problemset/problem/61/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define HA cout << "YES" << '\n';
+#define NA cout << "NO" << '\n';
+#define nl '\n'
+#define mod 1000000007
+#define N 500005
+ll fx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+ll fy[] = {1, -1, 0, 0, -1, 1, -1, 1};
+
+int main()
+{
+    FAST;
+    string a, b;
+    cin >> a >> b;
+
+    int n = a.size();
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] != b[i])
+            cout << '1';
+        else
+            cout << '0';
+    }
+    cout << '\n';
+    return 0;
+}
