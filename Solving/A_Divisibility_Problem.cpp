9d37325d2584253745502348247b044ecbff5e53
ER Pranto <erpranto55@gmail.com>
2025-12-21 23:59:30 +0600

    A_Divisibility_Problem

---
+++ Solving/A_Divisibility_Problem.cpp
@@ -0,0 +1,30 @@
+//https://codeforces.com/problemset/problem/1328/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define HA cout << "YES" << '\n';
+#define NA cout << "NO" << '\n';
+#define nl '\n'
+#define mod 1000000007
+#define N 500005
+ll fx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+ll fy[] = {1, -1, 0, 0, -1, 1, -1, 1};
+
+int main()
+{
+    FAST;
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll a, b;
+        cin >> a >> b;
+        cout << (b - a % b) % b << nl;
+    }
+    return 0;
+}
