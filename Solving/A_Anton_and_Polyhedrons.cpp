9d5cbe6c61940f277673a26f3f496dd608f6be07
ER Pranto <erpranto55@gmail.com>
2026-01-02 23:21:05 +0600

    A_Anton_and_Polyhedrons

---
+++ Solving/A_Anton_and_Polyhedrons.cpp
@@ -0,0 +1,41 @@
+//https://codeforces.com/problemset/problem/785/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define HA cout << "YES" << '\n';
+#define NA cout << "NO" << '\n';
+#define nl '\n'
+#define mod 1000000007
+#define N 500005
+ll fx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+ll fy[] = {1, -1, 0, 0, -1, 1, -1, 1};
+
+int main()
+{
+    FAST;
+    int n;
+    cin >> n;
+
+    unordered_map<string, int> faces = {
+        {"Tetrahedron", 4},
+        {"Cube", 6},
+        {"Octahedron", 8},
+        {"Dodecahedron", 12},
+        {"Icosahedron", 20}};
+
+    int total = 0;
+    while (n--)
+    {
+        string s;
+        cin >> s;
+        total += faces[s];
+    }
+
+    cout << total << nl;
+    return 0;
+}
