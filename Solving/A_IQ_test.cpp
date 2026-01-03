8f9b5c9164c6c6aed931532ac0c873137d342065
ER Pranto <erpranto55@gmail.com>
2026-01-03 20:04:54 +0600

    A_IQ_test

---
+++ Solving/A_IQ_test.cpp
@@ -0,0 +1,45 @@
+// https://codeforces.com/problemset/problem/25/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define HA cout << "YES" << '\n';
+#define NA cout << "NO" << '\n';
+#define nl '\n'
+#define mod 1000000007
+#define N 500005
+ll fx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+ll fy[] = {1, -1, 0, 0, -1, 1, -1, 1};
+
+int main()
+{
+    FAST;
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (a[i] % 2 == 0)
+            cnt++;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if ((cnt >= 2 && a[i] % 2 != 0) ||
+            (cnt < 2 && a[i] % 2 == 0))
+        {
+            cout << i + 1 << nl;
+            break;
+        }
+    }
+    return 0;
+}
