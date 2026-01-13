17f84a7cc3642886bc8862f2b32b981b148abbf6
ER Pranto <erpranto55@gmail.com>
2026-01-13 23:27:06 +0600

    A_Dubstep

---
+++ Solving/A_Dubstep.cpp
@@ -0,0 +1,43 @@
+//https://codeforces.com/problemset/problem/208/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define HA cout << "YES" << '\n';
+#define NA cout << "NO" << '\n';
+#define nl '\n'
+#define mod 1000000007
+#define N 500005
+ll fx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+ll fy[] = {1, -1, 0, 0, -1, 1, -1, 1};
+
+int main()
+{
+    FAST;
+    string s;
+    cin >> s;
+    for (int i = 0; i + 2 < s.size();)
+    {
+        if (s.substr(i, 3) == "WUB")
+        {
+            s.replace(i, 3, " ");
+            i++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    stringstream ss(s);
+    string wrd;
+
+    while (ss >> wrd)
+    {
+        cout << wrd << " ";
+    }
+    return 0;
+}
