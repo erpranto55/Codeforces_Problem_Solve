2f4a9dc15b0b6b91a1558d851d8903a504101176
erpranto <erpranto55@gmail.com>
2026-01-27 23:07:56 +0600

    A_Games

---
+++ Solving/A_Games.cpp
@@ -0,0 +1,34 @@
+//https://codeforces.com/problemset/problem/268/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> home(n), away(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> home[i] >> away[i];
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j && home[i] == away[j])
+            {
+                count++;
+            }
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
