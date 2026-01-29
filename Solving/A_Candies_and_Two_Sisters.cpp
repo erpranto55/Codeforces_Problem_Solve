98caa6c70b49f9817347ae0088f336843f6ce56c
erpranto <erpranto55@gmail.com>
2026-01-29 23:14:49 +0600

    A_Candies_and_Two_Sisters

---
+++ Solving/A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,33 @@
+//https://codeforces.com/problemset/problem/1335/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define HA cout << "YES" << '\n';
+#define NA cout << "NO" << '\n';
+#define nl '\n'
+#define mod 1000000007
+#define N 500005
+ll fx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+ll fy[] = {1, -1, 0, 0, -1, 1, -1, 1};
+
+int main()
+{
+    FAST;
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll n;
+        cin >> n;
+        if (n <= 2)
+            cout << 0 << nl;
+        else
+            cout << (n - 1) / 2 << nl;
+    }
+    return 0;
+}
