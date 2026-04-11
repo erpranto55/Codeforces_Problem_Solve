867e6934503649d1f02dfdd29e0712016c9b2d8a
erpranto <erpranto55@gmail.com>
2026-04-11 23:47:38 +0600

    A_Sum

---
+++ Solving/A_Sum.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define HA cout << "YES" << '\n';
+#define NA cout << "NO" << '\n';
+#define nl '\n'
+#define mod 1000000007
+#define N 500005
+ll fx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+ll fy[] = {1, -1, 0, 0, -1, 1, -1, 1};
+
+int main()
+{
+    FAST;
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        bool flag = false;
+        if (a + b == c || b + c == a || a + c == b)
+        {
+            flag = true;
+        }
+        if (flag)
+            HA else NA
+    }
+    return 0;
+}
